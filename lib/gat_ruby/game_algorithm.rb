79002053e241b301601ee85038e1cacfa1511a5b
paulocheque <paulocheque@gmail.com>
2013-08-07 22:24:25 -0300

    [new] algorithm with log system

--- lib/gat_ruby/game_algorithm.rb
+++ lib/gat_ruby/game_algorithm.rb
@@ -1,29 +1,57 @@
+require "logger"
 require "socket"
 require "json"
 
 # algorithm = GameAlgorithm.new
 # algorithm.listen
 class GameAlgorithm
-  def log(message)
-    puts "[GATRuby] #{message}"
+  def initialize
+    @logger = Logger.new(STDOUT)
+    config_log
+  end
+
+  def config_log
+    @logger.formatter = proc do |severity, datetime, progname, msg|
+      "#{msg}\n"
+    end
+    @logger.level = Logger::INFO
+    custom_log_level = nil
+    custom_log_level = ARGV[1].to_i if ARGV.count > 1
+    if custom_log_level
+      if custom_log_level == 10
+        @logger.level = Logger::DEBUG
+      elsif custom_log_level == 20
+        @logger.level = Logger::INFO
+      elsif custom_log_level == 30
+        @logger.level = Logger::WARN
+      elsif custom_log_level == 40
+        @logger.level = Logger::ERROR
+      elsif custom_log_level == 50
+        @logger.level = Logger::FATAL
+      end
+    end
+  end
+
+  def log(message, log_level=Logger::INFO)
+    @logger.add(log_level) { "[GATRuby] #{message}" }
   end
 
   def listen(host='localhost', port=nil)
     unless port
       port = ARGV[0].to_i if ARGV.count > 0
     end
-    puts "Random port: #{port}"
+    log "Random port: #{port}", Logger::DEBUG
     @server = TCPServer.open(port)
-    puts "Listening on port #{port}"
+    log "Listening on port #{port}", Logger::DEBUG
     @client = @server.accept
-    puts "Client connected"
+    log "Client connected", Logger::DEBUG
 
     @stopped = false
     while not @stopped
       begin
         read_incoming_message
       rescue Exception => e
-        puts e
+        log e, Logger::ERROR
         send_error(e)
         stop
         raise e
